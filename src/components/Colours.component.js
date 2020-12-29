74e4c9986e77e7bba7ffd43eea007adf5e89751c
Ben <ben.lynch1931@gmail.com>
2020-12-29 13:01:02 +0000

    console.log response

--- src/components/Colours.component.js
+++ src/components/Colours.component.js
@@ -65,8 +65,9 @@ const ColourComponent = () => {
           // 'Access-Control-Allow-Origin': 'https://knitting-chart.vercel.app/'
         }
       })
-      .then(res => res.json())
-      .then(data => formatColours(data.colours))
+      .then(res => console.log(res))
+      // .then(res => res.json())
+      // .then(data => formatColours(data.colours))
     }, []);
 
 
